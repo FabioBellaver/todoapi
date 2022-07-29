be1ad52efb5d3ff8b25e4a113720644006a09866
unknown <fabio.bellaver658@gmail.com>
2022-07-29 19:39:46 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const cors = require("cors");
+
+const { v4: uuidv4 } = require("uuid");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const users = [];
+
+function checksExistsUserAccount(request, response, next) {
+  // Complete aqui
+}
+
+app.post("/users", (request, response) => {
+  const { name, username } = request.body;
+
+  const userExists = users.find((user) => user.username === username);
+
+  if (userExists) {
+    return response.status(400).json({ error: "User already exists" });
+  }
+
+  const user = {
+    id: uuidv4(),
+    name,
+    username,
+    todos: [],
+  };
+  users.push(user);
+
+  return response.status(201).json(user);
+});
+
+app.get("/todos", checksExistsUserAccount, (request, response) => {
+  const { username } = request.headers;
+  return username.todos;
+});
+
+app.post("/todos", checksExistsUserAccount, (request, response) => {
+  // Complete aqui
+});
+
+app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
+  // Complete aqui
+});
+
+app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
+  // Complete aqui
+});
+
+app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
+  // Complete aqui
+});
+
+module.exports = app;
